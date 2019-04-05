135dace3bd1427e16aa84e5c30cb8c1ad1ce28b6
Dionos <40255376+aldanerking@users.noreply.github.com>
2019-04-05 15:35:56 -0300

    0.02
    Diseño principal del nav

---
+++ resources/views/Template/layoutTrasMel.Blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TrasMel</title>
+
+	<!-- Materilezed CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Boostrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+<!-- Barra Superior -->		
+<div class="Container-fluid">
+	<nav class="navbar navbar-dark bg-dark">
+		<div class="col-md-3">
+			<a href="#!" class="">TrasMel</a>
+		</div>
+		<div class="col-md-7">
+		<ul id="nav-mobile" class="left hide-on-med-and-down">
+        	<li><a href="#">Sass</a></li>
+        	<li><a href="#">Components</a></li>
+        	<li><a href="#">JavaScript</a></li>
+     	</ul>
+		</div>
+		<div class="col-md-2">
+			Holi mUndo
+		</div>
+	</nav>
+</div>
+
+<!-- Materialized Scripts -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+<!-- Boostrap Script -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/TrasMel', function () {
+    return view('Template/layoutTrasMel');
+});
