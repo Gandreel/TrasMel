3d0d8a3ded56404bd54a8d50d954944e6418fd56
Dionos <40255376+aldanerking@users.noreply.github.com>
2019-04-05 18:41:17 -0300

    0.07.1

---
+++ resources/views/Template/layoutTrasMel.Blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TrasMel</title>
+ 
+ <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+            
+</head>
+<body style="background: #6c4675">
+	
+<!-- Barra Superior -->		
+<div class="container-fluid">
+<nav class="navbar-fixed">
+    <div class="nav-wrapper" style="background: #26004D">
+    	<div class="row">
+    		<div class="col s12 m4 l2">
+      			<a href="#" class="brand-logo">TrasMel</a>
+      		</div>
+      		<div class="nav-content col s12 m4 l8">
+      			<ul class="rigtt hide-on-med-and-down ">
+        			<li class="tab"><a href="#test1">Test 1</a></li>
+        			<li class="tab"><a class="active" href="#test2">Test 2</a></li>
+        			<li class="tab disabled"><a href="#test3">Disabled Tab</a></li>
+        			<li class="tab"><a href="#test4">Test 4</a></li>
+      			</ul>
+    		</div>
+      		<div class="col s12 m4 l2">
+      			Hola Miundo
+      		</div>
+    	</div>
+    </div>
+</nav>
+</div>
+
+<!--  Carusel -->
+<div>
+		
+</div>
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
